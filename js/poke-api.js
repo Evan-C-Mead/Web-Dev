50b8a38567102ebab47cf714346097de141f94f5
Evan Mead <evan.c.mead@gmail.com>
2021-04-09 18:40:06 -0500

    going to implement React instead

--- js/poke-api.js
+++ js/poke-api.js
@@ -2,16 +2,6 @@
     "use strict";
 
 // url for Poke API
-    // const url = "https://pokeapi.co/api/v2/pokemon";
-    //
-    // const allPokemon = () => fetch(url)
-    //     .then(result => result.json())
-    //     .catch(console.error);
-    //
-    // const onePokemon = (id) => fetch('${url}/${id}')
-    //     .then(result => result.json())
-    //     .catch(console.error)
-
     const Pokedex = require('pokedex-promise-v2');
     const options = {
         protocol: 'https',
